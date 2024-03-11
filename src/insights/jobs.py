dba644f809c1864cf7c91194bd33102aef27a339
Mariana Pereira <marianapereira.s@live.com>
2024-03-10 23:45:13 -0300

    Iniciando o projeto :rocket:

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            file_reader = csv.DictReader(file, delimiter=',', quotechar='"')
+
+            header, *data = file_reader
+            self.jobs_list = data
 
     def get_unique_job_types(self) -> List[str]:
         pass
