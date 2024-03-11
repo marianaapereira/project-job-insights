cf661dec54a7eb105e49bb5278240df7cf80fee8
Mariana Pereira <marianapereira.s@live.com>
2024-03-11 00:49:38 -0300

    Implementação do método get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,11 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        i = "industry"
+        unique_industries = list()
+
+        for industry in self.jobs_list:
+            if i in industry and industry[i]:
+                unique_industries.append(industry[i])
+
+        return set(unique_industries)
