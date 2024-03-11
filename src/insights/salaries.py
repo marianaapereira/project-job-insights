ab36517773dd3f4dd08ae50afe9318fabeb51e54
Mariana Pereira <marianapereira.s@live.com>
2024-03-11 01:31:49 -0300

    Implementação dos métodos get_max_salary e get_min_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,30 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        ms = "max_salary"
+        max_value = 0
+
+        for job in self.jobs_list:
+            if job[ms] and job[ms].isdigit():
+                int_ms = int(job[ms])
+
+                if int_ms > max_value:
+                    max_value = int_ms
+
+        return max_value
 
     def get_min_salary(self) -> int:
-        pass
+        ms = "min_salary"
+        min_value = 999999
+
+        for job in self.jobs_list:
+            if job[ms] and job[ms].isdigit():
+                int_ms = int(job[ms])
+
+                if int_ms < min_value:
+                    min_value = int_ms
+
+        return min_value
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
