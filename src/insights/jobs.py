2b92ad87d23b95a83e277292e15274f96aa9e733
Mariana Pereira <marianapereira.s@live.com>
2024-03-11 00:34:28 -0300

    Implementação do método filter_by_multiple_criteria

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -25,5 +25,18 @@ class ProcessJobs:
 
         return set(unique_job_types)
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, jobs, criteria) -> List[dict]:
+        industry = "industry"
+        job_type = "job_type"
+        found_criteria = False
+        filtered_jobs = list()
+
+        for job in jobs:
+            if job[industry] == criteria[industry] and job[job_type] == criteria[job_type]:
+                filtered_jobs.append(job)
+                found_criteria = True
+
+        if not found_criteria:
+            raise TypeError("Filter provided is not a dictionary")
+
+        return filtered_jobs
