1dc7fd467e21844a67a6640277e9540346835a79
Mariana Pereira <marianapereira.s@live.com>
2024-03-12 14:22:08 -0300

    Correção de lint para o requisito 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,9 +4,10 @@ PATH = 'data/jobs.csv'
 WORD_PY = 'Python'
 WORD_JS = 'Javascript'
 
+
 def test_counter():
     'Returns the correct number of ocurrences of "Python"'
-    assert count_ocurrences(PATH, WORD_PY) is 1639
+    assert count_ocurrences(PATH, WORD_PY) == 1639
 
     'Returns the correct number of ocurrences of "Javascript"'
-    assert count_ocurrences(PATH, WORD_JS) is 122
+    assert count_ocurrences(PATH, WORD_JS) == 122
