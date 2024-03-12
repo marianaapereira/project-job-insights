461702a820ea320b0dc2f0b547d48464613fbe6d
Mariana Pereira <marianapereira.s@live.com>
2024-03-12 14:18:18 -0300

    Implementação de testes para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
+PATH = 'data/jobs.csv'
+WORD_PY = 'Python'
+WORD_JS = 'Javascript'
 
 def test_counter():
-    raise
+    'Returns the correct number of ocurrences of "Python"'
+    assert count_ocurrences(PATH, WORD_PY) is 1639
+
+    'Returns the correct number of ocurrences of "Javascript"'
+    assert count_ocurrences(PATH, WORD_JS) is 122
